af61ade26ace0d8527d0ed5f33fc645ee3af78ae
Paolo Arcipe <ptarrayo24@gmail.com>
2026-09-03 02:48:56 -0700

    modified:   .gitignore 	modified:   index.js 	deleted:    index.js 	modified:   package.json

--- index.js
+++ index.js
@@ -16,14 +16,36 @@ app.post("/students", (request, response) =>{
     response.send(newStudent);
 });
 
-app.patch ("/students/:index", (request, response) =>{
-    const studentIndex = request.params;
+app.patch("/students/:index", (request, response) => {
+    const studentIndex = Number(request.params.index);
     const updatedStudentData = request.body;
 
-})
+    if (Number.isNaN(studentIndex) || studentIndex < 0 || studentIndex >= students.length) {
+        return response.status(404).send({ message: "Student not found" });
+    }
+
+    students = students.map((student, index) =>
+        index === studentIndex ? { ...student, ...updatedStudentData } : student
+    );
+
+    response.send(students[studentIndex]);
+});
+
+app.delete("/students/:index", (request, response) => {
+    const studentIndex = Number(request.params.index);
+
+    if (Number.isNaN(studentIndex) || studentIndex < 0 || studentIndex >= students.length) {
+        return response.status(404).send({ message: "Student not found" });
+    }
+
+    const deletedStudent = students[studentIndex];
+    students = students.filter((_, index) => index !== studentIndex);
+    response.send({ deletedStudent });
+});
 
 app.delete("/students", (request, response) => {
-    
+    students = [];
+    response.send({ message: "All students deleted" });
 });
 
 app.listen(3000, () => {
